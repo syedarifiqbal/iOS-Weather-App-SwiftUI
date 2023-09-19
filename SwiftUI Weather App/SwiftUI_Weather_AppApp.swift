d96ca51acce3df0167ab48c35a1aa8f95d84ffcb
Arif Iqbal <arifiqbal@outlook.com>
2023-09-19 18:41:56 +0500

    Initial Commit

---
+++ SwiftUI Weather App/SwiftUI_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Weather_AppApp.swift
+//  SwiftUI Weather App
+//
+//  Created by Arif Iqbal on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
